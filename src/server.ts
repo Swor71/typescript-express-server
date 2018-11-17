66062b386f56caba3d1667d508d7479996007a1a
Marcin Swornowski <swor71@gmail.com>
2018-11-17 19:06:36 +0100

    Add boilerplate server code

---
+++ src/server.ts
@@ -0,0 +1,44 @@
+import * as express from 'express';
+import * as mongoose from 'mongoose';
+import * as bodyParser from 'body-parser';
+import * as compression from 'compression';
+import * as logger from 'morgan';
+import * as helmet from 'helmet';
+import * as cors from 'cors';
+
+// import routers
+
+// Server class
+
+class Server {
+  public app: express.Application;
+
+  constructor() {
+    this.app = express();
+    this.config();
+    this.routes();
+  }
+
+  public config() {
+    // set up mongoose
+    const MONGO_URI = 'mongodb://localhost/tes';
+    mongoose.connect(MONGO_URI || process.env.MONGODB_URI);
+
+    // config
+    this.app.use(bodyParser.json());
+    this.app.use(bodyParser.urlencoded({ extended: true }));
+    this.app.use(compression());
+    this.app.use(logger('dev'));
+    this.app.use(helmet());
+    this.app.use(cors());
+  }
+
+  public routes(): void {
+    const router: express.Router = express.Router();
+
+    this.app.use('/', router);
+    // this.app.use('/api/v1/posts', PostRouter);
+  }
+}
+
+export default new Server().app;
